0c022ad8a78e44ba26ab31dcc33c01ac848b55c4
Jan Weyrich <jan.weyrich@viadee.de>
2020-03-04 08:54:17 +0100

    resource dependecy

--- example.tf
+++ example.tf
@@ -6,6 +6,25 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-b374d5a5"
   instance_type = "t2.micro"
-  # vpc_security_group_ids = ["sg-0077..."]
-  # subnet_id = "subnet-923a..."
+  depends_on = [aws_s3_bucket.example] # EXPLICIT DEPENDENCY
+}
+
+# New resource for the S3 bucket our application will use.
+resource "aws_s3_bucket" "example" {
+  # NOTE: S3 bucket names must be unique across _all_ AWS accounts, so
+  # this name must be changed before applying this example to avoid naming
+  # conflicts.
+  bucket = "terraform-getting-started-guide"
+  acl    = "private"
+}
+
+resource "aws_eip" "ip" {
+    vpc = true
+    instance = aws_instance.example.id # IMPLICIT DEPENDENCY
+}
+
+# will be created in parallel with other resources
+resource "aws_instance" "another" {
+  ami           = "ami-b374d5a5"
+  instance_type = "t2.micro"
 }
