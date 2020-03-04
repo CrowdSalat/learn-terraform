38f64347cb31d01a71313cf8eb69842cd8b19f65
Jan Weyrich <jan.weyrich@viadee.de>
2020-03-04 08:26:30 +0100

    change infrastructure

--- example.tf
+++ example.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-2757f631"
+  ami           = "ami-b374d5a5"
   instance_type = "t2.micro"
   # vpc_security_group_ids = ["sg-0077..."]
   # subnet_id = "subnet-923a..."
